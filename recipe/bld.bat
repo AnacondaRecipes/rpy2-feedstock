e76352f2cc16986fe21dd21edd46208190132d8b
Kai Tietz <ktietz@anaconda.com>
2019-05-07 14:40:03 +0200

    Fixed merge issue

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -8,29 +8,9 @@ dlltool -d python%CONDA_PY%.def -l %CONDA_DEFAULT_ENV%\libs\libpython%CONDA_PY%.
 
 :: Create an empty library for msvcrt??? since CygwinCCompiler seems to
 :: think that linking to that is a good idea (it is not).
-<<<<<<< HEAD
-if "%CONDA_PY%" == "37" (
-  ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-else (
-  if "%CONDA_PY%" == "36" (
-    ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-  ) else (
-    if "%CONDA_PY%" == "35" (
-      ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr140.dll.a
-    ) else (
-      if "%CONDA_PY%" == "34" (
-        ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr120.dll.a
-      ) else (
-        ar cru %CONDA_DEFAULT_ENV%\libs\libmsvcr90.dll.a
-      )
-    )
-  )
-)
-=======
 ar cru %PREFIX%\libs\libmsvcr140.dll.a
 ar cru %PREFIX%\libs\libmsvcr120.dll.a
 ar cru %PREFIX%\libs\libmsvcr90.dll.a
->>>>>>> 3e26bf3d833872943556fc5abbe1a0c68e6f9e85
 
 gendef %PREFIX%\python%CONDA_PY%.dll - > python%CONDA_PY%.def
 dlltool -d python%CONDA_PY%.def -l %PREFIX%\lib\libpython%CONDA_PY%.dll.a
